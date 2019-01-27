14f7772e0e36bf80145a877a3f6f561fa4712d9c
Mark <mark@remarkablemark.org>
2019-01-27 08:04:36 +0000

    refactor(scenes): replace with constants & family-friendly message

--- src/scenes/GameOver.js
+++ src/scenes/GameOver.js
@@ -1,26 +1,25 @@
-import { SCENES } from '../constants';
+import { COLORS, FONTS, SCENES, SIZES } from '../constants';
 import { Scene } from 'phaser';
 
 export default class GameOver extends Scene {
   constructor() {
     super({ key: SCENES.GAME_OVER });
-    this.tryAgain = this.tryAgain.bind(this);
   }
 
   create() {
-    this.add.text(100, 200, 'DAN SUCKS', {
-      color: '#00ff00',
-      fontFamily: 'Arial',
-      fontSize: 64,
+    this.add.text(100, 200, 'TEDDY GOT HIT =(', {
+      color: COLORS.LOSE,
+      fontFamily: FONTS.DEFAULT,
+      fontSize: SIZES.MEDIUM,
     });
 
     this.add.text(100, 400, 'TRY AGAIN?', {
-      color: '#00ff00',
-      fontFamily: 'Arial',
-      fontSize: 64,
+      color: COLORS.LOSE,
+      fontFamily: FONTS.DEFAULT,
+      fontSize: SIZES.MEDIUM,
     });
 
-    this.input.once('pointerdown', this.tryAgain);
+    this.input.once('pointerdown', this.tryAgain, this);
   }
 
   tryAgain() {

1481bd9b03abaaa6d8731c32913a7441f291b738
Mark <mark@remarkablemark.org>
2019-01-27 13:46:10 -0500

    feat(scenes): resize screen on game init in Boot

--- src/scenes/Boot.js
+++ src/scenes/Boot.js
@@ -6,6 +6,28 @@ export default class Boot extends Scene {
     super({ key: SCENES.BOOT });
   }
 
+  init() {
+    this.resize();
+  }
+
+  resize() {
+    const { canvas } = this.game;
+    const width =
+      window.innerWidth > 0 ? window.innerWidth : window.screen.width;
+    const height =
+      window.innerHeight > 0 ? window.innerHeight : window.screen.height;
+    const wratio = width / height;
+    const ratio = canvas.width / canvas.height;
+
+    if (wratio < ratio) {
+      canvas.style.width = width + 'px';
+      canvas.style.height = width / ratio + 'px';
+    } else {
+      canvas.style.width = height * ratio + 'px';
+      canvas.style.height = height + 'px';
+    }
+  }
+
   preload() {
     const { load } = this;
 
